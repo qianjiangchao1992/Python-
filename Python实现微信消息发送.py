bb7d8fc4c1b3f609f185f6ff810d23cf7b08b8dd
QJChao <41941999+qianjiangchao1992@users.noreply.github.com>
2018-08-09 15:52:52 +0800

    Update Python实现微信消息发送.py

--- Python实现微信消息发送.py
+++ Python实现微信消息发送.py
@@ -13,16 +13,17 @@ def get_news():
 def send_news():
     try:
         contents=get_news()
-    # 你朋友的微信名称，不是备注，也不是微信帐号。
+    # 你朋友的微信名称，账号与备注均可。
         friends=['name1','name2','name3','name3']
         for name in friends:
             my_friend = bot.friends().search(u'%s'%(name))[0]
-            my_friend.send(u"写个脚本试一试")
-            my_friend.send(u"开始")
+          # my_friend.send(u"写个脚本试一试")
+          # my_friend.send(u"开始")
+            my_friend.send(contents[0])
             my_friend.send(contents[1])
             your_name='yourname'
             my_friend.send(u"来自最爱你的朋友%s!"%(your_name))
-            my_friend.send(u"结束")
+          # my_friend.send(u"结束")
             time.sleep(5)
     # 每86400秒（1天），发送1次
     except:
